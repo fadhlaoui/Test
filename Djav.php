a923c3f156ca65edcb3351a00f628a6418cf6a89
fadhlaoui <walid@ileotech.com>
2015-03-25 14:43:36 +0100

    test

---
+++ Djav.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo "etna";
+echo "bonjour";
+$s = 3; 
+$p = 2;
+$y = 7;
+$x = 5;
+ ?>
---
+++ Eip.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo "test";
+$test = 88;
+<<<<<<< HEAD
+echo "bonjour";
+=======
+
+
+
+>>>>>>> parent of fa8cbb4... commit test
+ ?>
